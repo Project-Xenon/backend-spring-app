0780108acb364300782006f27429d69fdc583c56
Deepankar Malhan <deepankarmalhan@gmail.com>
2018-04-20 22:59:34 -0400

    Added Gson for serialization/deserialization of objects into/from JSON

--- src/main/java/xenon/HelloController.java
+++ src/main/java/xenon/HelloController.java
@@ -1,13 +1,24 @@
 package xenon;
 
+import com.google.gson.Gson;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
 
+    private static Gson GSON_OBJ = new Gson();
     @RequestMapping("/")
     public String index() {
-        return "Hello World!";
+        TestObject test = new TestObject();
+
+        String objJSON = GSON_OBJ.toJson(test);
+        return objJSON;
+    }
+
+    class TestObject {
+        String message = "Hello World?";
+
+        public TestObject() {}
     }
 }
