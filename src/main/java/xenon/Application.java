8345f3cf8db6835f34960b861dc8d3c875643d69
Deepankar Malhan <deepankarmalhan@gmail.com>
2018-04-25 13:02:30 -0400

    Got JDBC to work, does some transactions to show the capabilities. These should be removed and used in the API instead

--- src/main/java/xenon/Application.java
+++ src/main/java/xenon/Application.java
@@ -1,23 +1,65 @@
 package xenon;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import xenon.models.CustomerModel;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner{
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-            System.out.println("The application is running!");
-        };
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+//    @Bean
+//    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+//        return args -> {
+//            System.out.printf("The application is running %s!", name);
+//        };
+//    }
+
+    public void run(String... strings) throws Exception {
+
+        log.info("Creating tables");
+
+        //jdbcTemplate.execute("DROP TABLE customers IF EXISTS");
+        jdbcTemplate.execute("CREATE TABLE customers(" +
+                "id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
+
+        // Split up the array of whole names into an array of first/last names
+        List<Object[]> splitUpNames = Arrays.asList("John Woo", "Jeff Dean", "Josh Bloch", "Josh Long").stream()
+                .map(name -> name.split(" "))
+                .collect(Collectors.toList());
+
+        // Use a Java 8 stream to print out each tuple of the list
+        splitUpNames.forEach(name -> log.info(String.format("Inserting customer record for %s %s", name[0], name[1])));
+
+        // Uses JdbcTemplate's batchUpdate operation to bulk load data
+        jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES (?,?)", splitUpNames);
+
+        log.info("Querying for customer records where first_name = 'Josh':");
+        jdbcTemplate.query(
+                "SELECT id, first_name, last_name FROM customers WHERE first_name = ?", new Object[] { "Josh" },
+                (rs, rowNum) -> new CustomerModel(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name"))
+        ).forEach(customer -> log.info(customer.toString()));
     }
 }
