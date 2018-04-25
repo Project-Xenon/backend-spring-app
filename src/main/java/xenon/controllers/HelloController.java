4c899f30a6d6733748c480796dc8a1db160cbbeb
Deepankar Malhan <deepankarmalhan@gmail.com>
2018-04-25 15:42:58 -0400

    Added gradle wrapper to git

--- src/main/java/xenon/controllers/HelloController.java
+++ src/main/java/xenon/controllers/HelloController.java
@@ -1,13 +1,19 @@
-package xenon;
+package xenon.controllers;
 
 import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     private static Gson GSON_OBJ = new Gson();
+
     @RequestMapping("/")
     public String index() {
         TestObject test = new TestObject();
@@ -16,9 +22,8 @@ public class HelloController {
         return objJSON;
     }
 
+
     class TestObject {
         String message = "Hello World?";
-
-        public TestObject() {}
     }
 }
